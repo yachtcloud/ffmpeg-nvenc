d6523ece4f32acdb4333dc6f6c11afd5571abfb2
robert <robert@yachtcloud.eu>
2020-09-01 14:42:37 +0200

    TEMP: Change obs+websocket to featurebranch

--- build.sh
+++ build.sh
@@ -205,7 +205,7 @@ BuildOBS() {
             wget https://cdn-fastly.obsproject.com/downloads/cef_binary_3770_linux64.tar.bz2
         fi
         tar -xjf ./cef_binary_3770_linux64.tar.bz2
-        git clone --recursive https://github.com/yachtcloud/obs-studio.git
+        git clone --recursive -b feature_media-stall https://github.com/yachtcloud/obs-studio.git
 	    cd obs-studio
         mkdir -p build && cd build
         cmake -DUNIX_STRUCTURE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$build_dir" DCEF_ROOT_DIR="../../cef_binary_3770_linux64" ..
@@ -220,7 +220,7 @@ BuildOBS() {
         mkdir -p build && cd build
         make -j${cpus}
     else
-        git clone --recursive https://github.com/yachtcloud/obs-websocket.git
+        git clone --recursive -b feature_media-stall https://github.com/yachtcloud/obs-websocket.git
         cd obs-websocket
         mkdir build && cd build
         cmake -DLIBOBS_INCLUDE_DIR=${source_dir}/obs-studio/libobs -DCMAKE_INSTALL_PREFIX="$build_dir" -DUSE_UBUNTU_FIX=true ..
